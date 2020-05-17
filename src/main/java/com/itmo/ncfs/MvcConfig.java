74d52a583f17ae12a0101608d21e899284bd08eb
evpDev <vasilisk0071@yandex.ru>
2020-05-17 19:31:44 +0300

    Moderator page for displaying feedbacks is created

--- src/main/java/com/itmo/ncfs/MvcConfig.java
+++ src/main/java/com/itmo/ncfs/MvcConfig.java
@@ -9,5 +9,6 @@ public class MvcConfig implements WebMvcConfigurer {
 
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/hello").setViewName("hello");
+        registry.addViewController("/moderator").setViewName("moderator");
     }
 }
