bd0c52bfb8e741ed489c1798561d60471d21f503
evpDev <vasilisk0071@yandex.ru>
2020-05-18 02:26:33 +0300

    Moderation functionality is implemented

--- src/main/java/com/itmo/ncfs/MvcConfig.java
+++ src/main/java/com/itmo/ncfs/MvcConfig.java
@@ -10,5 +10,6 @@ public class MvcConfig implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/hello").setViewName("hello");
         registry.addViewController("/moderator").setViewName("moderator");
+        registry.addViewController("/moderate").setViewName("moderate");
     }
 }
